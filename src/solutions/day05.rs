5797f653a12a0db59dd5eb6975ac1ed390278f70
Zak-Temperton <zakbarrytemperton@gmail.com>
2022-12-07 10:44:47 +1300

    extract functions

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -13,53 +13,49 @@ fn setup(lines: &mut std::str::Lines) -> Vec<Vec<char>> {
     stacks
 }
 
+fn get_params(line: &str, stacks: &Vec<Vec<char>>) -> (usize, usize, usize, usize) {
+    let mut split = line.split_whitespace().skip(1).step_by(2);
+    let amount = split.next().unwrap().parse::<usize>().unwrap();
+    let from = split.next().unwrap().parse::<usize>().unwrap() - 1;
+    let to = split.next().unwrap().parse::<usize>().unwrap() - 1;
+    let from_len = stacks[from].len();
+    (amount, from, to, from_len)
+}
+
+fn get_result(stacks: Vec<Vec<char>>) -> String {
+    let mut res = String::new();
+    for s in stacks {
+        res.push(*(s.last().unwrap()) as char);
+    }
+    res
+}
+
 pub(crate) fn part1(text: &str) -> String {
     let mut lines = text.lines();
     let mut stacks = setup(&mut lines);
     for line in lines.skip(2) {
-        let mut split = line.split_whitespace().skip(1).step_by(2);
-
-        let amount = split.next().unwrap().parse::<usize>().unwrap();
-        let from = split.next().unwrap().parse::<usize>().unwrap() - 1;
-        let to = split.next().unwrap().parse::<usize>().unwrap() - 1;
-
-        let from_len = stacks[from].len();
+        let (amount, from, to, from_len) = get_params(line, &stacks);
         for i in 1..amount + 1 {
             let tmp = stacks[from][from_len - i];
             stacks[to].push(tmp);
         }
         stacks[from].truncate(from_len - amount);
     }
-    let mut res = String::new();
-    for s in stacks {
-        res.push(*(s.last().unwrap()));
-    }
-    res
+    get_result(stacks)
 }
 
 pub(crate) fn part2(text: &str) -> String {
     let mut lines = text.lines();
     let mut stacks = setup(&mut lines);
     for line in lines.skip(2) {
-        let mut split = line.split_whitespace().skip(1).step_by(2);
-
-        let amount = split.next().unwrap().parse::<usize>().unwrap();
-        let from = split.next().unwrap().parse::<usize>().unwrap() - 1;
-        let to = split.next().unwrap().parse::<usize>().unwrap() - 1;
-
-        let from_len = stacks[from].len();
+        let (amount, from, to, from_len) = get_params(line, &stacks);
         for i in (1..amount + 1).rev() {
             let tmp = stacks[from][from_len - i];
             stacks[to].push(tmp);
         }
         stacks[from].truncate(from_len - amount);
     }
-
-    let mut res = String::new();
-    for s in stacks {
-        res.push(*(s.last().unwrap()) as char);
-    }
-    res
+    get_result(stacks)
 }
 
 #[allow(soft_unstable, unused_imports, dead_code)]
