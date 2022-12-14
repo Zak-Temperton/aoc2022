f0e5f620ee30581e5a8fef77c1224a58c3bd7cfb
Zak-Temperton <zakbarrytemperton@gmail.com>
2022-12-14 14:40:34 +1300

    factored (usize, usize) as Pos

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,16 +1,19 @@
-fn init_map(text: &str) -> (Vec<Vec<u8>>, (usize, usize), (usize, usize)) {
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Pos(usize, usize);
+
+fn init_map(text: &str) -> (Vec<Vec<u8>>, Pos, Pos) {
     let mut map = Vec::new();
-    let mut start = (0, 0);
-    let mut end = (0, 0);
+    let mut start = Pos(0, 0);
+    let mut end = Pos(0, 0);
     for (y, line) in text.lines().enumerate() {
         let mut bytes = line.as_bytes().to_vec();
         for (x, b) in bytes.iter_mut().enumerate() {
             if *b == b'S' {
                 *b = b'a';
-                start = (x, y);
+                start = Pos(x, y);
             } else if *b == b'E' {
                 *b = b'z';
-                end = (x, y);
+                end = Pos(x, y);
             }
         }
         map.push(bytes);
@@ -20,12 +23,12 @@ fn init_map(text: &str) -> (Vec<Vec<u8>>, (usize, usize), (usize, usize)) {
 
 fn test_cell(
     map: &[Vec<u8>],
-    cell: &(usize, usize),
-    new_pos: (usize, usize),
+    cell: &Pos,
+    new_pos: Pos,
     cells: &mut [Vec<bool>],
-    end: (usize, usize),
+    end: Pos,
     steps: usize,
-    new: &mut Vec<(usize, usize)>,
+    new: &mut Vec<Pos>,
 ) -> Option<usize> {
     if map[cell.1][cell.0] + 1 >= map[new_pos.1][new_pos.0] && !cells[new_pos.1][new_pos.0] {
         if new_pos == end {
@@ -37,12 +40,7 @@ fn test_cell(
     None
 }
 
-fn bfs(
-    map: &[Vec<u8>],
-    mut start: Vec<(usize, usize)>,
-    end: (usize, usize),
-    cells: &mut [Vec<bool>],
-) -> usize {
+fn bfs(map: &[Vec<u8>], mut start: Vec<Pos>, end: Pos, cells: &mut [Vec<bool>]) -> usize {
     let mut steps = 0;
     while !start.is_empty() {
         let mut new = Vec::new();
@@ -50,14 +48,14 @@ fn bfs(
         for i in [-1, 1] {
             for cell in start.iter() {
                 if !(cell.0 == 0 && i == -1 || cell.0 == map[0].len() - 1 && i == 1) {
-                    let new_pos = ((cell.0 as isize + i) as usize, cell.1);
+                    let new_pos = Pos((cell.0 as isize + i) as usize, cell.1);
                     if let Some(value) = test_cell(map, cell, new_pos, cells, end, steps, &mut new)
                     {
                         return value;
                     }
                 }
                 if !(cell.1 == 0 && i == -1 || cell.1 == map.len() - 1 && i == 1) {
-                    let new_pos = (cell.0, (cell.1 as isize + i) as usize);
+                    let new_pos = Pos(cell.0, (cell.1 as isize + i) as usize);
                     if let Some(value) = test_cell(map, cell, new_pos, cells, end, steps, &mut new)
                     {
                         return value;
@@ -72,10 +70,10 @@ fn bfs(
 
 fn test_cell_b(
     cells: &mut [Vec<bool>],
-    new_pos: (usize, usize),
+    new_pos: Pos,
     map: &[Vec<u8>],
-    starts: &mut Vec<(usize, usize)>,
-    new: &mut Vec<(usize, usize)>,
+    starts: &mut Vec<Pos>,
+    new: &mut Vec<Pos>,
 ) {
     if !cells[new_pos.1][new_pos.0] {
         cells[new_pos.1][new_pos.0] = true;
@@ -87,22 +85,18 @@ fn test_cell_b(
     }
 }
 
-fn find_b(
-    map: &[Vec<u8>],
-    mut start: Vec<(usize, usize)>,
-    cells: &mut [Vec<bool>],
-) -> Vec<(usize, usize)> {
+fn find_b(map: &[Vec<u8>], mut start: Vec<Pos>, cells: &mut [Vec<bool>]) -> Vec<Pos> {
     let mut starts = Vec::new();
     while !start.is_empty() {
         let mut new = Vec::new();
         for i in [-1, 1] {
             for cell in start.iter() {
                 if !(cell.0 == 0 && i == -1 || cell.0 == map[0].len() - 1 && i == 1) {
-                    let new_pos = ((cell.0 as isize + i) as usize, cell.1);
+                    let new_pos = Pos((cell.0 as isize + i) as usize, cell.1);
                     test_cell_b(cells, new_pos, map, &mut starts, &mut new);
                 }
                 if !(cell.1 == 0 && i == -1 || cell.1 == map.len() - 1 && i == 1) {
-                    let new_pos = (cell.0, (cell.1 as isize + i) as usize);
+                    let new_pos = Pos(cell.0, (cell.1 as isize + i) as usize);
                     test_cell_b(cells, new_pos, map, &mut starts, &mut new);
                 }
             }
